43d1fbfd8b17abd054acc0eb036a88578dab11ba
SandroIerna <iernasandro@gmail.com>
2023-01-17 16:30:49 +0100

    adds blog fetching

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 
 const BlogList = (props) => {
+  const [blogs, setBlogs] = useState();
+
+  const getBlogs = async () => {
+    try {
+      const res = await fetch("http://localhost:3001/blogs");
+      if (res.ok) {
+        const data = await res.json();
+        setBlogs(data);
+      } else {
+        console.log("Something went wrong");
+      }
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getBlogs();
+  }, []);
   return (
     <Row>
-      {posts.map((post) => (
-        <Col
-          md={4}
-          style={{
-            marginBottom: 50,
-          }}
-        >
-          <BlogItem key={post.title} {...post} />
-        </Col>
-      ))}
+      {blogs &&
+        blogs.map((blog) => (
+          <Col
+            md={4}
+            style={{
+              marginBottom: 50,
+            }}
+          >
+            <BlogItem key={blog._id} {...blog} />
+          </Col>
+        ))}
     </Row>
   );
 };
